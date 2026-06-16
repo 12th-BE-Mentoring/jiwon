f3a3e9a21957582916d8f84b6f930ede4acdfebb
jiwonn43 <jiwonever4@gmail.com>
2026-06-16 22:19:38 +0900

    chore ::project setting

---
+++ src/test/java/com/example/jiwon/JiwonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jiwon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiwonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
