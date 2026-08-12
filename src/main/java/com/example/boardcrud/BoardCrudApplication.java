2cbbcdfe45b225d51abcb4bc83d3e2297c423d66
jiwonn43 <jiwonever4@gmail.com>
2026-08-12 19:29:05 +0900

    수정

--- src/main/java/com/example/boardcrud/BoardCrudApplication.java
+++ src/main/java/com/example/boardcrud/BoardCrudApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan; // import 추가!
 
-@ConfigurationPropertiesScan // <--- 바로 여기에 붙여주는 겁니다!
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class BoardCrudApplication {
 
