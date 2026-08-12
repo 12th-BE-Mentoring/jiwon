9f4dfee0f8b7d17de800c5af4efbd4c4ef9930bb
jiwonn43 <jiwonever4@gmail.com>
2026-08-12 19:50:21 +0900

    feat ::global 추가

--- src/main/java/com/example/boardcrud/global/error/ErrorCode.java
+++ src/main/java/com/example/boardcrud/global/error/ErrorCode.java
@@ -9,9 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),
 
-    BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "Board Not Found"),
-
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User Not Found");
+    BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "Board Not Found");
 
     private final HttpStatus status;
     private final String message;
